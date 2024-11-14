d52300e61b0de985dfeb9d74da42e74267b6d198
Lqnk4 <internalbeast109@gmail.com>
2024-11-14 03:09:36 -0800

    fixed build

--- src/main/java/frc/robot/commands/swervedrive/drivebase/PathFindToTag.java
+++ src/main/java/frc/robot/commands/swervedrive/drivebase/PathFindToTag.java
@@ -1,29 +1,21 @@
 package frc.robot.commands.swervedrive.drivebase;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.path.GoalEndState;
-import com.pathplanner.lib.path.PathConstraints;
-import com.pathplanner.lib.path.PathPlannerPath;
+import java.util.Optional;
+
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-
 public class PathFindToTag {
-//	public static Command pathFindToOffset(Pose2d pose, Transform2d offset) {
-//		PathConstraints constraints = new PathConstraints(Constants.MAX_SPEED, Constants.MAX_ACCELERATION, Constants.MAX_ANGULAR_VELOCITY, Constants.MAX_ANGULAR_ACCELERATION);
-//		return AutoBuilder.pathfindToPose(pose.plus(offset), constraints, 0.0, 0.0);
-//	}
-	public static Command pathFindToTote(SwerveSubsystem drivebase) {
-		Optional<Transform2d> bestTagOffset = photonVision.getBestTagTransform(Constants.VisionConstants.TOTE_TAG_FILTER);
-
-
-	}
+    /**
+     * Command to pathfind to an AprilTag on a Tote
+     */
+    public static Command pathFindToTote(SwerveSubsystem drivebase) {
+		Optional<Pose2d> toteDestinationPose = drivebase.getToteDestinationPose();
+		if(toteDestinationPose.isPresent()) {
+			return drivebase.driveToPose(toteDestinationPose.get());
+		}
+		return new WaitCommand(4);
+    }
 }
